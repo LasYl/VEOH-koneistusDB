21050d6abb3b9d021d0491eb3b47d4267b5fef86
LasYl <56217678+LasYl@users.noreply.github.com>
2020-05-09 09:08:58 +0300

    controller päivitetty

--- machining_controller.js
+++ machining_controller.js
@@ -1,19 +1,16 @@
 //tietokantamalli, joka kaivaa tiedon tietokannasta, ja muuntaa sen halutuksi muodoksi
 
 // määritetään viitaten tiedostoon material_model
-const material_model = require('./material_model')
+const machining_model = require('./machining_model')
 
 //HELPERS
-const material_data = (req) => {
+const machining_data = (req) => {
     //data talteen, määritetään luotavat arvot
     let data = {
-        name: req.body.name,
-        min_density: req.body.min_density,
-        max_density: req.body.max_density,
-        min_strength: req.body.min_strength,
-        max_strength: req.body.max_strength,
-        min_strength_density: req.body.min_strength / req.body.max_density,
-        max_strength_density: req.body.max_strength / req.body.min_density
+        tool_name: req.body.tool_name,
+        material: req.body.material,
+        cutting_speed: req.body.cutting_speed,
+        feed_rate: req.body.feed_rate,
 
     };
 
@@ -23,18 +20,18 @@ const material_data = (req) => {
 
 //CREATE
 // yksi uusi materiaali
-const api_post_material = (req, res, next) => {
-    console.log('api_post_material');
+const api_post_machining = (req, res, next) => {
+    console.log('api_post_machining');
     // Apufunktiolla kutsutaan jo luotuHELPERSIIN  data
-    let data = material_data(req)
+    let data = machining_data(req)
   
 
-    let new_material = material_model(data);
+    let new_machining = machining_model(data);
 
-    new_material.save().then(()=>{
-        console.log(new_material);
+    new_machining.save().then(()=>{
+        console.log(new_machining);
         //lähetetään sama data takaisin JSON.stringify-muodossa
-        res.send(JSON.stringify(new_material));
+        res.send(JSON.stringify(new_machining));
     }).catch(err => {
         res.status(500);
         //lähettää virheen postmanohjelman bodyyn
@@ -51,13 +48,13 @@ const api_post_material = (req, res, next) => {
 
 //lukukäsittelijä, uusi funktio
 // lista kaikista materiaaleista. (s perässä)
-const api_get_materials = (req, res, next) => {
+const api_get_machinings = (req, res, next) => {
 
-    material_model.find({})
+    machining_model.find({})
     .lean()
-    .then(materials => {
+    .then(machinings => {
     //palautetaan tmaterials, että nähdään sen tulevan oikealle käsittelijälle
-    res.send(JSON.stringify(materials));  
+    res.send(JSON.stringify(machinings));  
     }).catch(err => {
         res.status(500);
         //lähettää virheen postmanohjelman bodyyn
@@ -68,16 +65,16 @@ const api_get_materials = (req, res, next) => {
   }; 
 
 //UPDATE
-const api_put_material = (req, res, next) => {
+const api_put_machining = (req, res, next) => {
     console.log('sdbbsd')
     let id = req.params.id;
     // Apufunktiolla kutsutaan jo luotuHELPERSIIN  data
-    let data = material_data(req)
+    let data = machining_data(req)
 
-    material_model.findByIdAndUpdate(id, data,{
+    machining_model.findByIdAndUpdate(id, data,{
         new:true
-    }).then( (material)=>{
-        res.send(material);
+    }).then( (machining)=>{
+        res.send(machining);
     }).catch(err => {
         res.status(500);
         //lähettää virheen postmanohjelman bodyyn
@@ -89,10 +86,10 @@ const api_put_material = (req, res, next) => {
 
 //DELETE
 
-const api_delete_material = (req, res, next) => {
+const api_delete_machining = (req, res, next) => {
     // haetaan IDksi kyseinen id mongosta
     let id = req.params.id;
-    material_model.findByIdAndRemove(id).then(()=>{
+    machining_model.findByIdAndRemove(id).then(()=>{
         res.send();
         console.log('poistettu');
 
@@ -106,7 +103,7 @@ const api_delete_material = (req, res, next) => {
 }
 
 //EXPORTS
-module.exports.api_post_material = api_post_material;
-module.exports.api_get_materials = api_get_materials;
-module.exports.api_delete_material = api_delete_material;
-module.exports.api_put_material = api_put_material;
+module.exports.api_post_machiningl = api_post_machining;
+module.exports.api_get_machinings = api_get_machinings;
+module.exports.api_delete_machining = api_delete_machining;
+module.exports.api_put_machining = api_put_machining;
